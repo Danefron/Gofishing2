55f1f8c5379b266fb0f34e5dfd5b16e335690cc9
Danefron <86188096+Danefron@users.noreply.github.com>
2023-03-30 17:16:50 +0530

    Update Config.cs

--- src/PluginTemplate/Config.cs
+++ src/PluginTemplate/Config.cs
@@ -10,9 +10,12 @@ internal class Config
 // Token: 0x17000001 RID: 1
 // (get) Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
 // (set) Token: 0x06000002 RID: 2 RVA: 0x00002058 File Offset: 0x00000258
-public class catch { get; set; } = new catch rewards[]
+public class CatchRewards
 {
-new catch reward()
+    public CatchRewards [] Rewards
+{ get; set; } = new
+CatchRewards [] { new
+CatchRewards () };
 };
 
 // Token: 0x06000003 RID: 3 RVA: 0x00002064 File Offset: 0x00000264
